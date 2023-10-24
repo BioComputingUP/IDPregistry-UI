b7fe75744179a11ffe56321b6c62b89e406a48c1
Ivan Mičetić <ivan.micetic@gmail.com>
2023-10-24 14:25:02 +0200

    Default route points to '' instead of /home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,21 +1,17 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from "./pages/home/home.component";
-import { SparqlComponent } from './pages/sparql/sparql.component';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {HomeComponent} from "./pages/home/home.component";
+import {SparqlComponent} from './pages/sparql/sparql.component';
 
 const routes: Routes = [
-  {
-    path : '',
-    redirectTo : '/home',
-    pathMatch : 'full',
-  },
-  {path : 'home', component : HomeComponent},
-  {path : 'sparql', component : SparqlComponent},
+  {path: '', component: HomeComponent},
+  {path: 'sparql', component: SparqlComponent},
+  {path: '**', redirectTo: '/', component: HomeComponent}
 ];
 
 @NgModule({
-  imports : [RouterModule.forRoot(routes)],
-  exports : [RouterModule],
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
 })
 export class AppRoutingModule {
 }
